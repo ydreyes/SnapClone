858918280c8d8b004941d6d451294381caee582a
David <yandridavidr@gmail.com>
2025-11-13 14:10:40 -0500

    Feat: ported for mobiles

--- Assets/Scripts/SceneManager/MainMenuController.cs
+++ Assets/Scripts/SceneManager/MainMenuController.cs
@@ -4,5 +4,5 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
 	public void OnPlay()  => SceneManager.LoadScene("CharacterSelect");
-	public void OnQuit()  { UnityEditor.EditorApplication.isPlaying = false; Application.Quit(); }
+	//public void OnQuit()  { UnityEditor.EditorApplication.isPlaying = false; Application.Quit(); }
 }
