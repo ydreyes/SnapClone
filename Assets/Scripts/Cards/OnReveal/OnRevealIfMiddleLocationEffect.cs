f2baf7663d9ae4b7ab663e3954aaac3ed3161f3f
David <yandridavidr@gmail.com>
2025-12-18 10:41:17 -0500

    Feat: Onreveal Effects of AntMan And Medusa Added

---
+++ Assets/Scripts/Cards/OnReveal/OnRevealIfMiddleLocationEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu( fileName = "OnRevealIfMiddleLocation", menuName = "Cards/OnReveal/If Middle Location +Power" )]
+public class OnRevealIfMiddleLocationEffect : CardEffectBase
+{
+	public int bonusPower = 3;
+	
+	public override void ApplyEffect(CardInstance card, Zone zone)
+	{
+		if (card == null || zone == null)
+		{
+			return;
+		}
+		
+		// Zona central
+		Zone middleZone = GameManager.Instance.zones[1];
+		
+		if (zone != middleZone)
+		{
+			return;
+		}
+		
+		// apply the bonus power
+		card.currentPower += bonusPower;
+		card.UpdatePowerUI();
+		zone.UpdatePowerDisplay();
+		
+		Debug.Log(
+			$"[OnReveal] {card.data.cardName} está en la zona central y gana +{bonusPower}"
+		);
+	}
+}
--- Assets/Scripts/Cards/Ongoing/OngoingIfSideFullEffect.cs
+++ Assets/Scripts/Cards/Ongoing/OngoingIfSideFullEffect.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class OngoingIfSideFullEffect : MonoBehaviour
+[CreateAssetMenu(
+	fileName = "OngoingIfSideFull", menuName = "Cards/Ongoing/If Side Full +Power"
+)]
+
+public class OngoingIfSideFullEffect : CardEffectBase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public int bonusPower = 4;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public override void ApplyEffect(CardInstance card, Zone zone)
+	{
+		if (card == null || zone == null)
+			return;
+
+		// Determinar el lado correcto
+		int cardsOnSide = card.isPlayerCard
+			? zone.playerCards.Count
+			: zone.aiCards.Count;
+
+		bool sideIsFull = cardsOnSide >= 4;
+
+		// Poder base
+		int basePower = card.data.power;
+
+		if (sideIsFull)
+		{
+			if (card.currentPower != basePower + bonusPower)
+			{
+				card.currentPower = basePower + bonusPower;
+				card.UpdatePowerUI();
+				zone.UpdatePowerDisplay();
+			}
+		}
+		else
+		{
+			if (card.currentPower != basePower)
+			{
+				card.currentPower = basePower;
+				card.UpdatePowerUI();
+				zone.UpdatePowerDisplay();
+			}
+		}
+	}
 }
